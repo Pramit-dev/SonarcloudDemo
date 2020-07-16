c9e0db36a04c087246c10dbaa35d59abb570c5a0
Sharma <Pramit.Sharma@centurylink.com>
2020-07-16 16:49:27 +0530

    first commit

---
+++ src/main/java/com/pramit/sonarclouddemo/SonarcloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pramit.sonarclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramit/sonarclouddemo/SonarcloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramit.sonarclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarcloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
